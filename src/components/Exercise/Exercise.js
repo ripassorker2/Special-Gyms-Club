8bde28ed9c512fa92a574963eb7a140633441ca4
Ripassorker <ripassorker02@glail.com>
2022-09-29 01:06:31 +0600

    added user info dynamic

--- src/components/Exercise/Exercise.js
+++ src/components/Exercise/Exercise.js
@@ -3,15 +3,12 @@ import './Exercise.css'
 
 const Exercise = (props) => {
     const { picture, time, price, name } = props.exercise;
-
     return (
         <div>
-
             <div className='exercise-container'>
                 <img src={picture} alt="" />
                 <div>
                     <h3>{name}</h3>
-
                     <p><strong>Price : ${price}</strong></p>
                     <p><strong>Time : {time}s</strong></p>
                     <button> Add to List </button>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -10,8 +10,27 @@ const Home = () => {
                     <Exercises></Exercises>
                 </div>
                 <div className="cart-container">
-                    <h1>i am cart</h1>
+                    <div className='user-info'>
+                        <img src="https://scontent.fdac20-1.fna.fbcdn.net/v/t1.6435-9/118290441_229293018817549_5675367518733679984_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=174925&_nc_ohc=m6vroJvxC_4AX-xgCJh&tn=7_wcj_6NfxzthbaP&_nc_ht=scontent.fdac20-1.fna&oh=00_AT93YWxDHHMgQVJU7C2lT0LWU4LOh3fjVtL0Wop2tzX14A&oe=6359803B" alt="" />
+                        <p>Ripas Sorker Rifat</p>
+                    </div>
+                    <div className='physical-info'>
+                        <h3>My physical info</h3>
+                        <div>
+                            <p>Weight : 53 kg</p>
+                            <p>Height : 5.7 inces</p>
+                            <p>Age : 53 years</p>
+                        </div>
+                    </div>
+                    <div className="breck-info">
+                        <p>10s</p>
+                        <p>20s</p>
+                        <p>30s</p>
+                        <p>40s</p>
+                        <p>50s</p>
+                    </div>
                 </div>
+
             </div>
 
         </div>
